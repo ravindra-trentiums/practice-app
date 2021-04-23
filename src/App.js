2409b5bd9d65b242f5443b926e849b2a0d6c73e8
ravindra-trentiums <ravindra@trentiums.com>
2021-04-23 09:37:20 +0530

    remove forgot password

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from "react";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-// import HomePage from "./views/HomePage.js";
 import LoginPage from "./views/LoginPage.js";
 import RegisterPage from "./views/RegisterPage.js";
-import ForgotPassword from "./views/forgotPasssword.js";
-// import AuthLoading from './views/AuthLoading';
 import { Provider } from "react-redux";
-// import ProtectedRoute from './views/ProtectedRoute'; 
-// import BookingHistory from './views/BookingHistory';
 import Footer from "./containers/footer"
 import Header from "./containers/header"
 import store from "./redux/store";
@@ -20,8 +15,6 @@ function App() {
       <BrowserRouter >
         <Header />
         <Switch>
-          {/* <ProtectedRoute exact component={BookingHistory} path="/bookings" permit={"rider,admin,driver"} /> */}
-          <Route exact path="/" component={ForgotPassword} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/register" component={RegisterPage} />
         </Switch>
